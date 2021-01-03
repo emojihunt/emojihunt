1ddae3c342438dccb571ecc6e120caf2d91e3021
Gaurav Singh <gs@gauravjs.com>
2021-01-02 18:45:02 -0800

    Try #2 fixing Channel linking.

--- huntbot/huntbot.go
+++ huntbot/huntbot.go
@@ -37,8 +37,8 @@ func (h *HuntBot) NewPuzzle(ctx context.Context, name string) error {
 	// Update Spreadsheet with channel URL, spreadsheet URL.
 
 	// Post a message in the general channel with a link to the puzzle.
-	h.dis.GeneralChannelSend(fmt.Sprintf("There is a new puzzle %s!\nPuzzle URL: %s\nChannel #%s",
-		name, puzzleURL, strings.ToLower(name)))
+	h.dis.GeneralChannelSend(fmt.Sprintf("There is a new puzzle %s!\nPuzzle URL: %s\nChannel #%s <#%s>",
+		name, puzzleURL, strings.ToLower(name), id))
 	// Pin a message with the spreadsheet URL to the channel
 	h.dis.ChannelSendAndPin(id, fmt.Sprintf("Spreadsheet: %s\nPuzzle: %s", sheetURL, puzzleURL))
 	return nil
