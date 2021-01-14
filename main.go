f2370d12a3a06d40613bdae54a50c57bb969e859
Gaurav Singh <gs@gauravjs.com>
2021-01-13 22:03:02 -0800

    Cleanup discord voice handler.

--- main.go
+++ main.go
@@ -99,7 +99,7 @@ func main() {
 	dis.RegisterNewMessageHandler("isithuntyet?", huntyet.Handler)
 	dis.RegisterNewMessageHandler("bot control", h.ControlHandler)
 	dis.RegisterNewMessageHandler("qm manager", dis.QMHandler)
-	dis.RegisterNewMessageHandler("voice controller", dis.VoiceChannelHandler)
+	dis.RegisterNewMessageHandler("voice controller", h.VoiceChannelHandler)
 
 	go h.WatchSheet(ctx)
 
