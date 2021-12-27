c64fd4533a27ccbdd385be0a91734e7dd1e92d18
Ben Kraft <ben@benkraft.org>
2021-12-27 15:08:01 -0500

    reorganize a little

--- discord/discord.go
+++ discord/discord.go
@@ -300,37 +300,6 @@ func (c *Client) CreateChannel(name string) (string, error) {
 	return ch.ID, nil
 }
 
-func (c *Client) QMHandler(s *discordgo.Session, m *discordgo.MessageCreate) error {
-	if m.Author.ID == s.State.User.ID || !strings.HasPrefix(m.Content, "!qm") || m.ChannelID != c.qmChannelID {
-		return nil
-	}
-
-	var reply string
-	var err error
-	switch m.Content {
-	case "!qm start":
-		if err = s.GuildMemberRoleAdd(c.guildID, m.Author.ID, c.qmRoleID); err != nil {
-			reply = fmt.Sprintf("unable to make %s a QM: %v", m.Author.Mention(), err)
-			break
-		}
-		reply = fmt.Sprintf("%s is now a QM", m.Author.Mention())
-	case "!qm stop":
-		if err = s.GuildMemberRoleRemove(c.guildID, m.Author.ID, c.qmRoleID); err != nil {
-			reply = fmt.Sprintf("unable to remove %s from QM role: %v", m.Author.Mention(), err)
-			break
-		}
-		reply = fmt.Sprintf("%s is no longer a QM", m.Author.Mention())
-	default:
-		err = fmt.Errorf("unexpected QM command: %q", m.Content)
-		reply = fmt.Sprintf("unexpected command: %q\nsupported qm commands are \"!qm start\" and \"!qm stop\"", m.Content)
-	}
-	if err != nil {
-		log.Printf("error setting QM: %v", err)
-	}
-	_, err = s.ChannelMessageSend(m.ChannelID, reply)
-	return err
-}
-
 func (c *Client) ClosestRoomID(input string) (string, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
