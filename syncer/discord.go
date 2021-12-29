b6c2f632d02eb750ed6d1f78a70a01c38171d4b6
Benjamin Tidor <benjamin@tidor.net>
2021-12-29 13:02:20 -0800

    document discordcreateupdatepin

--- syncer/discord.go
+++ syncer/discord.go
@@ -9,6 +9,10 @@ import (
 
 const pinnedStatusHeader = "Puzzle Information"
 
+// DiscordCreateUpdatePin creates or updates the pinned message at the top of
+// the puzzle channel. This message contains information about the puzzle status
+// as well as links to the puzzle and the spreadsheet. It needs to be updated
+// when the puzzle status changes.
 func (s *Syncer) DiscordCreateUpdatePin(puzzle *schema.Puzzle) error {
 	embed := &discordgo.MessageEmbed{
 		Author: &discordgo.MessageEmbedAuthor{Name: pinnedStatusHeader},
