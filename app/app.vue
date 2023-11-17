7d29832a715095c0eae3e9410cca969fb1581e63
Benjamin Tidor <benjamin@tidor.net>
2023-11-16 17:44:04 -0800

    Implement data loading for homepage

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,44 @@
+<script setup lang="ts">
+type Puzzle = {
+  id: number,
+  name: string,
+  answer: string,
+  round: Round,
+  status: "" | "Working" | "Abandoned" | "Solved" | "Backsolved",
+  description: string,
+  location: string,
+  puzzle_url: string,
+  spreadsheet_id: string,
+  discord_channel: string,
+  original_url: string,
+  name_override: string,
+  archived: boolean,
+  voice_room: string,
+};
+
+type Round = {
+  id: number,
+  name: string,
+  emoji: string,
+};
+
+const { data: puzzles } = <{ data: Ref<Puzzle[]>; }>await useFetch("http://localhost:8000/puzzles");
+const { data: rounds } = <{ data: Ref<Round[]>; }>await useFetch("http://localhost:8000/rounds");
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <h1>Rounds</h1>
+  <ol>
+    <li v-for="round in rounds">{{ round.emoji }} {{ round.name }}</li>
+  </ol>
+
+  <h1>Puzzles</h1>
+  <ol>
+    <li v-for="puzzle in puzzles">
+      {{ puzzle.round.emoji }}
+      <a :href="puzzle.puzzle_url">
+        {{ puzzle.name }}
+      </a>
+    </li>
+  </ol>
 </template>
