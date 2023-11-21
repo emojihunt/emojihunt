b8831f185a3665dd9d3729de0c99e76288a40177
Benjamin Tidor <benjamin@tidor.net>
2023-11-21 10:24:23 -0800

    JS: simplify app config

--- app/app.config.prod.ts
+++
@@ -1,4 +0,0 @@
-export default defineAppConfig({
-  apiBase: 'https://api.emojihunt.tech',
-  clientID: '1058094051586490368',
-});
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,8 +1,18 @@
+const prod = process.env.NODE_ENV == "production";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  appConfig: {
+    clientID: prod ? '1058094051586490368' : '1058094051586490368',
+  },
   css: [
     "assets/normalize.css",
     "assets/main.css",
   ],
   devtools: { enabled: true },
-})
+  nitro: {
+    devProxy: {
+      "/api": "http://localhost:8080"
+    },
+  },
+});
