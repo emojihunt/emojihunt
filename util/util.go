76f77c6f1b7466f0865519e418b590aaccdd8cee
Benjamin Tidor <benjamin@tidor.net>
2023-11-20 17:24:02 -0800

    API: remove unused InvalidPuzzle struct

--- util/util.go
+++ util/util.go
@@ -6,13 +6,6 @@ import (
 
 var BostonTime = MustLoadLocation("America/New_York")
 
-type InvalidPuzzle struct {
-	ID       int64
-	Name     string
-	Problems []string
-	EditURL  string
-}
-
 func MustLoadLocation(name string) *time.Location {
 	location, err := time.LoadLocation("America/New_York")
 	if err != nil {
