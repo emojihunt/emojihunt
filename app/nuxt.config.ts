677717de1e27f68ea3fd298299ecd40b671c11c9
Benjamin Tidor <benjamin@tidor.net>
2023-11-20 22:17:06 -0800

    Frontend: add fonts and reset stylesheet

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    "assets/normalize.css",
+    "assets/main.css",
+  ],
   devtools: { enabled: true },
   ssr: false,
 })
