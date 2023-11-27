c8e3c638ce4ca4b64c58d8e23d52467c595d3495
Benjamin Tidor <benjamin@tidor.net>
2023-11-26 21:00:55 -0800

    JS: add column titles to puzzle list

--- app/components/PuzzleListHeader.vue
+++ app/components/PuzzleListHeader.vue
@@ -4,20 +4,25 @@ const hue = props.puzzles[0].round.color;
 </script>
 
 <template>
-  <header>
+  <header class="pill">
     <div class="emoji">{{ puzzles[0].round.emoji }}&#xfe0f;</div>
     <div class="round">{{ puzzles[0].round.name }}</div>
     <div class="progress">
       {{ puzzles.filter((p: any) => !!p.answer).length }}/{{ puzzles.length }}
     </div>
   </header>
+  <header class="titles">
+    <div class="cell">Status &bull; Answer</div>
+    <div class="cell">Location</div>
+    <div class="cell">Note</div>
+  </header>
 </template>
 
 <style scoped>
 /* Layout */
-header {
-  width: 83%;
+.pill {
   grid-column: 1 / 3;
+  width: 83%;
   margin: 2.4rem 0 0.5rem;
   display: flex;
 
@@ -25,8 +30,15 @@ header {
   line-height: 2.25rem;
 }
 
+.titles {
+  grid-column: 3 / 6;
+  display: grid;
+  grid-template-columns: subgrid;
+  align-self: flex-end;
+}
+
 /* Themeing */
-header {
+.pill {
   font-size: 1.07rem;
   padding: 0 1rem;
 
@@ -50,5 +62,13 @@ header {
   flex-grow: 1;
   text-align: right;
   font-variant-numeric: diagonal-fractions;
+}
+
+.titles {
+  padding-bottom: 0.2rem;
+
+  font-size: 0.8rem;
+  font-weight: 430;
+  color: oklch(55% 0 0deg);
 }
 </style>
