245f04a8374d7310a6c6d01e23893e0a189518f9
Benjamin Tidor <benjamin@tidor.net>
2023-12-06 12:06:58 -0800

    JS: split out navbar emoji component, add color

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -33,12 +33,7 @@ onMounted(() => document.location.hash && history.pushState(
 <template>
   <header>
     <section class="rounds" v-if="rounds.length > 2">
-      <span v-for="round of rounds">
-        <a :href="`#${round.anchor}`" @click="navigate">
-          {{ round.emoji }}&#xfe0f;
-        </a>
-        <label v-if="round.complete">✔</label>
-      </span>
+      <NavbarEmoji v-for="round of rounds" :round="round" :navigate="navigate" />
     </section>
   </header>
 </template>
@@ -61,69 +56,13 @@ header {
   gap: 0.5rem;
 }
 
-span {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-a {
-  display: block;
-  width: 1.75rem;
-  line-height: 1.75rem;
-  height: 2rem;
-}
-
-label {
-  display: block;
-  width: 0.75rem;
-  height: 0.75rem;
-  margin-top: -0.33rem;
-}
-
 
 /* Themeing */
 header {
-  background-color: oklch(98% 0.01 286deg);
-  border-bottom: 1px solid oklch(80% 0.01 286deg);
+  background-color: oklch(98% 0 0deg);
+  border-bottom: 1px solid oklch(80% 0 0deg);
   filter: drop-shadow(0 1.5rem 1rem oklch(100% 0 0deg));
 
   user-select: none;
-}
-
-span {
-  opacity: 60%;
-  pointer-events: none;
-}
-
-a {
-  background-color: oklch(98% 0.01 286deg);
-  border: 1px solid transparent;
-  border-radius: 0.33rem;
-
-  text-align: center;
-  text-decoration: none;
-
-  pointer-events: auto;
-}
-
-span:hover {
-  opacity: 100%;
-}
-
-span:hover a {
-  border-color: oklch(80% 0.01 286deg);
-  filter: drop-shadow(0 1px 2px oklch(95% 0 0deg));
-}
-
-label {
-  color: oklch(60% 0.01 286deg);
-  font-size: 0.6rem;
-  text-align: center;
-
-  background-color: oklch(98% 0.01 286deg);
-  border-radius: 0.3rem;
-
-  z-index: 3;
 }
 </style>
