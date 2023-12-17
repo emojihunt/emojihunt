e8883600ff5eb55d5d30709301b0516e93db0131
Benjamin Tidor <benjamin@tidor.net>
2023-12-16 19:01:57 -0800

    JS: migrate to composables/ directory

---
+++ app/composables/useRovingTabIndex.ts
@@ -0,0 +1,41 @@
+// Set up the "roving tabindex" accessibility strategy. A single element in the
+// container will be selectable (tabIndex=0) while the others will not
+// (tabIndex=-1); the left and right keys change which element is selected.
+//
+// The parent container should have the `stop` class, the returned keydown
+// handler should be registered on it, and tab indexes should be set according
+// to the returned index variable.
+export default function (limit: number, start: number = 0):
+  [FocusInfo, (e: KeyboardEvent) => void] {
+
+  const state = reactive({ index: start });
+  const keydown = (e: KeyboardEvent) => {
+    if (e.key == "ArrowRight") {
+      if (state.index < limit - 1) state.index += 1;
+    } else if (e.key == "ArrowLeft") {
+      if (state.index > 0) state.index -= 1;
+    } else {
+      return;
+    }
+    const parent = getStopParent(document.activeElement);
+    // @ts-ignore
+    nextTick(() => parent?.querySelector("[tabindex='0']")?.focus());
+    e.preventDefault();
+  };
+  return [state, keydown];
+};
+
+
+// Gets the nearest parent of the given element that has the class "stop".
+export const getStopParent = (element: Element | null): Element | null => {
+  if (!element) {
+    return null;
+  } else if (element.classList.contains("stop")) {
+    return element;
+  } else {
+    return getStopParent(element.parentElement);
+  }
+};
+
+export const tabIndex = (focused: FocusInfo, target: number): number =>
+  focused.index == target ? 0 : -1;
---
+++ app/composables/useStickyIntersectionObserver.ts
@@ -0,0 +1,26 @@
+// Set up an IntersectionObserver to detect when elements with `position:
+// sticky` sticks to the top of the page. The observer signals sticking by
+// adding or removing the `stuck` class from the given element.
+//
+// Note: this helper does not work on Chrome if the element has a drop-shadow
+// filter. See: crbug.com/1358819.
+//
+export default function (margin: number): IntersectionObserver {
+  const callback: IntersectionObserverCallback = (entries) => {
+    // We get events when the element touches or un-touches the header *and*
+    // when it enters or exits the viewport from below. Check the
+    // y-coordinate to disambiguate.
+    for (const { boundingClientRect, isIntersecting, target } of entries) {
+      if (!isIntersecting && boundingClientRect.y < (margin + 1)) {
+        target.classList.add("stuck");
+      } else {
+        target.classList.remove("stuck");
+      }
+    }
+  };
+  return new IntersectionObserver(callback, {
+    root: null,
+    rootMargin: `-${margin}px 0px 0px 0px`,
+    threshold: 1.0,
+  });
+};
