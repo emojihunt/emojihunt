d4bce3746ffa23389d0e7d8caa31b6837e631136
Benjamin Tidor <benjamin@tidor.net>
2023-12-18 10:14:45 -0800

    JS: try out Heroicons for puzzle buttons

--- app/components/PuzzleButtons.vue
+++ app/components/PuzzleButtons.vue
@@ -12,122 +12,47 @@ const discordURL = props.puzzle.discord_channel.length > 1 ?
 
 <template>
   <nav>
-    <!-- Icons from OpenMoji, https://openmoji.org/. -->
-    <NuxtLink :href="puzzleURL" :ok="!!puzzleURL" :tabindex="tabIndex(focused, 0)">
-      <svg id="emoji" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg">
-        <g id="color">
-          <circle cx="36" cy="36" r="28" fill="#92D3F5" stroke="none" stroke-linecap="round"
-            stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-          <path fill="#B1CC33" stroke="none" stroke-linecap="round" stroke-linejoin="round"
-            stroke-miterlimit="10" stroke-width="2"
-            d="M41.6878,29.1965c-0.0568,0.7946-2.2706,3.5222-2.8378,3.9161c-1.1826,0.8214-1.6443,0.4449-2.6746,1.2557 c-0.653,0.5139-1.469,1.0634-2.4689,1.2557c-0.5534,0.1064-0.3086-0.4165-1.5537,0.3831c-0.6936,0.4454-0.0778,0.9247,0,1.3621 c0.1046,0.588,0.1828,0.2336,1.3621,1.3621c0.6157,0.5893,0.6707,1.1144,1.3621,1.3621c0.4322,0.1549,0.7463,0.07,1.3621,0 c1.2911-0.1467,0.717,1.1259,1.3621,1.3621C39.0851,41.9988,39.6331,41.7716,41,42c0.552,0.0922,1.3122-0.1799,2,1 c0.2963,0.5083,0,2,0.05,2.5421c0.0844,0.9159-0.5065,1.7288-1.3621,2.7243c-0.6921,0.8052-0.8206,0.6811-1.3621,1.3621 c-0.7236,0.9099-0.5299,1.1763-1.3621,2.7243c-0.8424,1.5667-1.3055,1.7862-1.3621,2.7243c-0.0237,0.3923-0.0011,1.32,0,1.3621 c0.0238,0.9465,0.0765,1.0716,0,1.3621c-0.1781,0.677-1.5528,1.2699-2.0716,1.1919c-1.0211-0.1535-1.0026-3.6818-1.05-4.2283 c-0.0958-1.1035,0.3564-1.7204,0.3973-2.4121c0.1412-2.393,0.5408-3.1351,0-4.0864c-0.3451-0.607-0.5273-0.3396-1.3621-1.3621 c-0.4715-0.5774-1.1856-1.4733-1.3621-2.7243c-0.1113-0.788,0.1212-0.7928,0-1.3621c-0.2251-1.0562-1.1013-1.3971-2.7243-2.7243 c-1.0013-0.8188-1.8894-1.5449-2.7243-2.7243c-0.4339-0.6128-1.0478-1.48-1.3621-2.7243c-0.4138-1.6378,0.2025-1.9051,0-4.0864 c-0.0457-0.4913-0.2445-2.2829-1.3621-4.0864c-0.4745-0.7657-0.8087-1.2882-1.3621-1.3621c-1.055-0.141-1.6211,1.519-2.7243,1.3621 c-0.6593-0.0938-1.1689-0.788-1.3621-1.3621c-0.0517-0.1534-0.2402-0.7406,0-1.3621c0.1331-0.3445,0.5428-0.6837,1.3621-1.3621 c0.836-0.6924,1.2563-1.045,1.3621-1.3621c0.1992-0.5974-0.1599-0.8105,0-1.3621c0.2735-0.944,1.7229-1.6939,2.7243-1.3621 c0.5752,0.1906,0.4511,0.5555,1.3621,1.3621c0.4423,0.3916,1.3981,1.1259,2.7243,1.3621c0.6697,0.1193,1.0045,0.1788,1.3621,0 c0.8809-0.4408,0.4597-1.6084,1.3621-2.7243c0.8301-1.0264,2.1787-1.2654,2.7243-1.3621c1.3486-0.2391,1.5329,0.2732,2.7243,0 c1.2802-0.2937,1.2433-0.9257,2.7243-1.3621c0.6485-0.1911,1.791-0.528,2.7243,0c0.8401,0.4752,0.7287,1.2093,1.3621,1.3621 c0.884,0.2134,1.3172-1.1646,2.7243-1.3621c1.0743-0.1508,2.3522,0.4375,2.7243,1.3621c0.1847,0.4589,0.0982,0.8817,0,1.3621 c-0.2966,1.4511-1.0836,1.5389-1.3621,2.7243c-0.3028,1.288,0.3458,1.6004,0,2.7243c-0.227,0.7378-0.6811,0.5108-1.4047,1.1067 c-0.3311,0.2727-0.4115,0.7094-1.3196,1.6175c-0.454,0.454-0.9255,0.8914-1.3621,1.3621 C42.0546,28.9072,41.8959,28.9885,41.6878,29.1965" />
-        </g>
-        <g id="line">
-          <circle cx="36" cy="36" r="28" fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-          <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
-            stroke-miterlimit="10" stroke-width="2"
-            d="M41.6878,29.1965c-0.0568,0.7946-2.2706,3.5222-2.8378,3.9161c-1.1826,0.8214-1.6443,0.4449-2.6746,1.2557 c-0.653,0.5139-1.469,1.0634-2.4689,1.2557c-0.5534,0.1064-0.3086-0.4165-1.5537,0.3831c-0.6936,0.4454-0.0778,0.9247,0,1.3621 c0.1046,0.588,0.1828,0.2336,1.3621,1.3621c0.6157,0.5893,0.6707,1.1144,1.3621,1.3621c0.4322,0.1549,0.7463,0.07,1.3621,0 c1.2911-0.1467,0.717,1.1259,1.3621,1.3621C39.0851,41.9988,39.6331,41.7716,41,42c0.552,0.0922,1.3122-0.1799,2,1 c0.2963,0.5083,0,2,0.05,2.5421c0.0844,0.9159-0.5065,1.7288-1.3621,2.7243c-0.6921,0.8052-0.8206,0.6811-1.3621,1.3621 c-0.7236,0.9099-0.5299,1.1763-1.3621,2.7243c-0.8424,1.5667-1.3055,1.7862-1.3621,2.7243c-0.0237,0.3923-0.0011,1.32,0,1.3621 c0.0238,0.9465,0.0765,1.0716,0,1.3621c-0.1781,0.677-1.5528,1.2699-2.0716,1.1919c-1.0211-0.1535-1.0026-3.6818-1.05-4.2283 c-0.0958-1.1035,0.3564-1.7204,0.3973-2.4121c0.1412-2.393,0.5408-3.1351,0-4.0864c-0.3451-0.607-0.5273-0.3396-1.3621-1.3621 c-0.4715-0.5774-1.1856-1.4733-1.3621-2.7243c-0.1113-0.788,0.1212-0.7928,0-1.3621c-0.2251-1.0562-1.1013-1.3971-2.7243-2.7243 c-1.0013-0.8188-1.8894-1.5449-2.7243-2.7243c-0.4339-0.6128-1.0478-1.48-1.3621-2.7243c-0.4138-1.6378,0.2025-1.9051,0-4.0864 c-0.0457-0.4913-0.2445-2.2829-1.3621-4.0864c-0.4745-0.7657-0.8087-1.2882-1.3621-1.3621c-1.055-0.141-1.6211,1.519-2.7243,1.3621 c-0.6593-0.0938-1.1689-0.788-1.3621-1.3621c-0.0517-0.1534-0.2402-0.7406,0-1.3621c0.1331-0.3445,0.5428-0.6837,1.3621-1.3621 c0.836-0.6924,1.2563-1.045,1.3621-1.3621c0.1992-0.5974-0.1599-0.8105,0-1.3621c0.2735-0.944,1.7229-1.6939,2.7243-1.3621 c0.5752,0.1906,0.4511,0.5555,1.3621,1.3621c0.4423,0.3916,1.3981,1.1259,2.7243,1.3621c0.6697,0.1193,1.0045,0.1788,1.3621,0 c0.8809-0.4408,0.4597-1.6084,1.3621-2.7243c0.8301-1.0264,2.1787-1.2654,2.7243-1.3621c1.3486-0.2391,1.5329,0.2732,2.7243,0 c1.2802-0.2937,1.2433-0.9257,2.7243-1.3621c0.6485-0.1911,1.791-0.528,2.7243,0c0.8401,0.4752,0.7287,1.2093,1.3621,1.3621 c0.884,0.2134,1.3172-1.1646,2.7243-1.3621c1.0743-0.1508,2.3522,0.4375,2.7243,1.3621c0.1847,0.4589,0.0982,0.8817,0,1.3621 c-0.2966,1.4511-1.0836,1.5389-1.3621,2.7243c-0.3028,1.288,0.3458,1.6004,0,2.7243c-0.227,0.7378-0.6811,0.5108-1.4047,1.1067 c-0.3311,0.2727-0.4115,0.7094-1.3196,1.6175c-0.454,0.454-0.9255,0.8914-1.3621,1.3621 C42.0546,28.9072,41.8959,28.9885,41.6878,29.1965" />
-        </g>
+    <!-- Icons from Heroicons, https://heroicons.com. -->
+    <NuxtLink :href="puzzleURL || '#'" :ok="!!puzzleURL" :tabindex="tabIndex(focused, 0)"
+      style="--hue: 238deg;">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64" />
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path fill-rule="evenodd"
+          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM6.262 6.072a8.25 8.25 0 1 0 10.562-.766 4.5 4.5 0 0 1-1.318 1.357L14.25 7.5l.165.33a.809.809 0 0 1-1.086 1.085l-.604-.302a1.125 1.125 0 0 0-1.298.21l-.132.131c-.439.44-.439 1.152 0 1.591l.296.296c.256.257.622.374.98.314l1.17-.195c.323-.054.654.036.905.245l1.33 1.108c.32.267.46.694.358 1.1a8.7 8.7 0 0 1-2.288 4.04l-.723.724a1.125 1.125 0 0 1-1.298.21l-.153-.076a1.125 1.125 0 0 1-.622-1.006v-1.089c0-.298-.119-.585-.33-.796l-1.347-1.347a1.125 1.125 0 0 1-.21-1.298L9.75 12l-1.64-1.64a6 6 0 0 1-1.676-3.257l-.172-1.03Z"
+          clip-rule="evenodd" />
       </svg>
     </NuxtLink>
-    <NuxtLink :href="spreadsheetURL" :ok="!!spreadsheetURL"
-      :tabindex="tabIndex(focused, 1)">
-      <svg id="emoji" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg">
-        <g id="color">
-          <polygon fill="#3F3F3F" stroke="none"
-            points="14.5943,53.154 18.2544,56.814 12.6242,59.0928" />
-          <polyline fill="#f4aa41" stroke="none"
-            points="18.0381,41.8761 36.8684,23.0457 48.1813,34.3586 29.5108,53.0291" />
-          <polyline fill="#a57939" stroke="none"
-            points="14.4132,52.5663 17.9281,43.3946 27.8278,53.2939 18.6558,56.809" />
-          <polyline fill="#EA5A47" stroke="none"
-            points="42.9209,16.9933 50.4228,9.4913 61.7357,20.8042 54.2975,28.2424" />
-          <polyline fill="#9b9b9a" stroke="none"
-            points="35.6498,24.2643 43.3318,16.5823 54.6447,27.8952 47.0278,35.512" />
-        </g>
-        <g id="line">
-          <polygon fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"
-            points="18.6304,56.8203 27.8278,53.2939 53.8207,27.301 43.9212,17.4015 17.9281,43.3946 14.3904,52.6032" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"
-            points="47.3354,13.9873 50.8388,10.4839 60.7383,20.3834 57.2645,23.8572" />
-          <line x1="18.5562" x2="28.4557" y1="42.7665" y2="52.6659" fill="none"
-            stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
-            stroke-miterlimit="10" stroke-width="2" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"
-            points="14.3979,52.5817 11.9073,59.3148 18.6558,56.809" />
-          <line x1="36.9099" x2="46.4225" y1="25.0073" y2="34.5199" fill="none"
-            stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
-            stroke-miterlimit="10" stroke-width="2" />
-        </g>
+    <NuxtLink :href="spreadsheetURL || '#'" :ok="!!spreadsheetURL"
+      :tabindex="tabIndex(focused, 1)" style="--hue: 148deg;">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path
+          d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+        <path
+          d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
       </svg>
     </NuxtLink>
-    <NuxtLink :href="discordURL" :ok="!!discordURL" :tabindex="tabIndex(focused, 2)">
-      <svg id="emoji" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg">
-        <g id="color">
-          <polygon fill="#8967AA"
-            points="10,21 10,35 13,35 13,38 16,38 16,45 22,45 22,51 27,51 28,51 28,45 44,45 44,51 50,51 50,45 56,45 56,38 59,38 59,35 62,35 62,20 56,20 56,24 56,25 49,25 49,20 45,20 45,14 39,14 39,20 32,20 32,14 26,14 26,20 22,20 22,25 16,25 16,20 10,20" />
-          <rect x="16" y="51" width="6" height="6" fill="#8967AA" />
-          <rect x="50" y="51" width="6" height="6" fill="#8967AA" />
-        </g>
-        <g id="line">
-          <rect x="25.175" y="31" width="3.6" height="6" stroke="#000000"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" points="22,45 16,45 16,39" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" points="22.583,25 22.583,20 26,20" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2"
-            points="48.708,25 48.708,20 45.292,20" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2"
-            points="13,35 10,35 10,20 16,20 16,35" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2"
-            points="56,35 56,20 62,20 62,35 59,35" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" points="26,20 26,14 32,14 32,20" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" points="39,20 39,14 45,14 45,20" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2"
-            points="16,35 19,35 19,38 13,38 13,35" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2"
-            points="59,35 59,38 53,38 53,35 56,35" />
-          <rect x="16" y="51" width="6" height="6" fill="none" stroke="#000000"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-          <rect x="50" y="51" width="6" height="6" fill="none" stroke="#000000"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" points="28,45 28,51 22,51 22,45" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" points="50,45 56,45 56,39" />
-          <polyline fill="none" stroke="#000000" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" points="44,45 44,51 50,51 50,45" />
-          <rect x="43.425" y="31" width="3.6" height="6" stroke="#000000"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-          <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" d="M44,45L44,45z" />
-          <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" d="M39,20L39,20z" />
-          <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" d="M16,25L16,25z" />
-          <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" d="M49,25L49,25z" />
-          <line x1="28" x2="44" y1="45" y2="45" fill="none" stroke="#000000"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-          <line x1="32" x2="39" y1="20" y2="20" fill="none" stroke="#000000"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-          <line x1="16" x2="22" y1="25" y2="25" fill="none" stroke="#000000"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-          <line x1="49" x2="56" y1="25" y2="25" fill="none" stroke="#000000"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-        </g>
+    <NuxtLink :href="discordURL || '#'" :ok="!!discordURL" :tabindex="tabIndex(focused, 2)"
+      style="--hue: 282deg;">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+        data-slot="icon" class="w-6 h-6">
+        <path
+          d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
+        <path
+          d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
       </svg>
     </NuxtLink>
   </nav>
@@ -147,10 +72,9 @@ a {
 
 svg {
   display: inline-block;
-  height: 1.55rem;
-  width: 1.75rem;
-  margin: 0.10rem 0;
-  filter: opacity(70%);
+  height: 1.5rem;
+  width: 1.8rem;
+  margin: 0.125rem 0;
 }
 
 /* Theming */
@@ -159,13 +83,25 @@ a {
 }
 
 a[ok="false"] {
-  filter: grayscale(100%) opacity(50%);
+  filter: grayscale(100%) opacity(60%);
   pointer-events: none;
 }
 
-a:hover svg {
-  transform: scale(110%);
-  filter: drop-shadow(0 2px 1px oklch(85% 0 0deg));
-  /* also clears prior opacity() filter */
+a {
+  color: oklch(42% 0.24 var(--hue));
+  stroke-width: 0.8px;
+}
+
+a svg:last-child {
+  display: none;
+  opacity: 66%;
+}
+
+a:hover svg:first-child {
+  display: none;
+}
+
+a:hover svg:last-child {
+  display: block;
 }
 </style>
