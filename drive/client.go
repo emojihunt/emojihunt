42cc75864f78abe4b25a6b4a9a54553882f4d41c
Benjamin Tidor <benjamin@tidor.net>
2024-01-04 12:26:46 -0800

    Go: add manual retry to Drive client

--- drive/client.go
+++ drive/client.go
@@ -3,11 +3,15 @@ package drive
 import (
 	"context"
 	"encoding/base64"
+	"errors"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"golang.org/x/xerrors"
 	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/googleapi"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
@@ -55,68 +59,78 @@ func NewClient(ctx context.Context, prod bool) *Client {
 }
 
 func (c *Client) CreateSheet(ctx context.Context, name string) (id string, err error) {
-	sheet, err := c.sheets.Spreadsheets.Create(&sheets.Spreadsheet{}).Context(ctx).Do()
+	sheet, err := withRetry("sheets.Create", func() (*sheets.Spreadsheet, error) {
+		return c.sheets.Spreadsheets.Create(&sheets.Spreadsheet{}).Context(ctx).Do()
+	})
 	if err != nil {
-		return "", xerrors.Errorf("sheets.Create (%q): %w", name, err)
+		return "", err
 	}
 	return sheet.SpreadsheetId, nil
 }
 
 func (c *Client) SetSheetTitle(ctx context.Context, sheetID, title string) error {
-	req := &sheets.BatchUpdateSpreadsheetRequest{
-		Requests: []*sheets.Request{{
-			UpdateSpreadsheetProperties: &sheets.UpdateSpreadsheetPropertiesRequest{
-				Fields: "title",
-				Properties: &sheets.SpreadsheetProperties{
-					Title: title,
+	_, err := withRetry("sheets.BatchUpdate", func() (*sheets.BatchUpdateSpreadsheetResponse, error) {
+		var req = &sheets.BatchUpdateSpreadsheetRequest{
+			Requests: []*sheets.Request{{
+				UpdateSpreadsheetProperties: &sheets.UpdateSpreadsheetPropertiesRequest{
+					Fields: "title",
+					Properties: &sheets.SpreadsheetProperties{
+						Title: title,
+					},
 				},
-			},
-		}},
-	}
-	_, err := c.sheets.Spreadsheets.
-		BatchUpdate(sheetID, req).
-		Context(ctx).
-		Do()
-	if err != nil {
-		return xerrors.Errorf("sheets.BatchUpdate (%s): %w", sheetID, err)
-	}
-	return nil
+			}},
+		}
+		return c.sheets.Spreadsheets.BatchUpdate(sheetID, req).Context(ctx).Do()
+	})
+	return err
 }
 
 func (c *Client) CreateFolder(ctx context.Context, name string) (id string, err error) {
-	file, err := c.drive.Files.Create(&drive.File{
-		Name:     name,
-		MimeType: "application/vnd.google-apps.folder",
-		Parents:  []string{c.rootFolderID},
-	}).Context(ctx).Do()
+	file, err := withRetry("drive.Files.Create", func() (*drive.File, error) {
+		return c.drive.Files.Create(&drive.File{
+			Name:     name,
+			MimeType: "application/vnd.google-apps.folder",
+			Parents:  []string{c.rootFolderID},
+		}).Context(ctx).Do()
+	})
 	if err != nil {
-		return "", xerrors.Errorf("drive.Files.Create (%s): %w", name, err)
+		return "", err
 	}
 	return file.Id, nil
 }
 
 func (c *Client) SetSheetFolder(ctx context.Context, sheetID, folderID string) error {
-	_, err := c.drive.Files.Update(sheetID, nil).
-		EnforceSingleParent(true).
-		AddParents(folderID).
-		Context(ctx).
-		Do()
-	if err != nil {
-		return xerrors.Errorf("drive.AddParents (%s): %w", sheetID, err)
-	}
-	return nil
+	_, err := withRetry("drive.AddParents", func() (*drive.File, error) {
+		return c.drive.Files.Update(sheetID, nil).EnforceSingleParent(true).
+			AddParents(folderID).Context(ctx).Do()
+	})
+	return err
 }
 
 func (c *Client) SetFolderName(ctx context.Context, folderID, name string) error {
-	_, err := c.drive.Files.Update(folderID, &drive.File{
-		Name:     name,
-		MimeType: "application/vnd.google-apps.folder",
-	}).EnforceSingleParent(true).
-		AddParents(c.rootFolderID).
-		Context(ctx).
-		Do()
-	if err != nil {
-		return xerrors.Errorf("drive.Files.Update (%s): %w", folderID, err)
+	_, err := withRetry("drive.Files.Update", func() (*drive.File, error) {
+		return c.drive.Files.Update(folderID, &drive.File{
+			Name:     name,
+			MimeType: "application/vnd.google-apps.folder",
+		}).EnforceSingleParent(true).AddParents(c.rootFolderID).Context(ctx).Do()
+	})
+	return err
+}
+
+func withRetry[T any](name string, request func() (T, error)) (result T, err error) {
+	for w := 5 * time.Second; w <= 30*time.Second; w += 5 * time.Second {
+		result, err = request()
+		if err == nil {
+			return
+		}
+		var ge *googleapi.Error
+		if ok := errors.As(err, &ge); !ok ||
+			!(ge.Code == http.StatusTooManyRequests || ge.Code >= 500) {
+			break
+		}
+		log.Printf("%s: retrying HTTP %d: %#v", name, ge.Code, ge.Message)
+		time.Sleep(w)
 	}
-	return nil
+	err = xerrors.Errorf("%s: %w", name, err)
+	return
 }
