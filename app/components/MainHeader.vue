b77b17e17370b558c9500e3920fed2532e879f73
Benjamin Tidor <benjamin@tidor.net>
2024-12-30 23:17:44 +0000

    JS: rework Ably connection indicator

--- app/components/MainHeader.vue
+++ app/components/MainHeader.vue
@@ -33,9 +33,14 @@ const [focused, keydown] = useRovingTabIndex(props.rounds.length);
       <EmojiNav v-for="round of rounds" :round="round" :observer-fixup="observerFixup"
         :selected="round.id === rounds[focused.index].id" />
     </nav>
-    <UTooltip class="ably" :text="connected ? 'Online' : 'Offline'" :open-delay="250"
-      :popper="{ placement: 'left', offsetDistance: 0 }">
-      <div class="dot" :class="connected && 'connected'"></div>
+    <UTooltip class="ably" text="Live updates paused. Connecting..." :open-delay="250"
+      :popper="{ placement: 'left', offsetDistance: 0 }" v-if="!connected">
+      <!-- Icon from Heroicons, https://heroicons.com. -->
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="m3 3 8.735 8.735m0 0a.374.374 0 1 1 .53.53m-.53-.53.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 0 1 0 5.304m2.121-7.425a6.75 6.75 0 0 1 0 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 0 1-1.06-2.122m-1.061 4.243a6.75 6.75 0 0 1-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12Z" />
+      </svg>
     </UTooltip>
   </header>
 </template>
@@ -60,15 +65,15 @@ nav {
 
 .ably {
   position: absolute;
-  top: 4.5rem;
+  top: 4rem;
   right: 0;
   padding: 0.5rem 1.25rem 0.5rem 0.5rem;
 }
 
-.dot {
+svg {
+  display: inline-block;
+  height: 1rem;
   width: 1rem;
-  height: 0.33rem;
-  border-radius: 0.25rem;
 }
 
 /* Theming */
@@ -79,13 +84,15 @@ header {
   user-select: none;
 }
 
-.dot {
-  background-color: oklch(62.5% 0.15 30deg);
-  filter: drop-shadow(0 0 5px oklch(75% 0.15 30deg));
+svg {
+  color: white;
+  opacity: 0;
 }
 
-.dot.connected {
-  background-color: oklch(55% 0.08 150deg);
-  filter: drop-shadow(0 0 3px oklch(65% 0.10 150deg / 50%));
+/* Animation */
+svg {
+  /* https://stackoverflow.com/a/16344389 */
+  animation: blink 1.5s step-start infinite;
+  animation-delay: 2.5s;
 }
 </style>
