45c033473c84015cc7a35f3e5840b937998c2f66
Benjamin Tidor <benjamin@tidor.net>
2025-01-05 19:32:45 -0800

    JS: Firefox now supports Clipboard API

--- app/components/RoundAndPuzzles.vue
+++ app/components/RoundAndPuzzles.vue
@@ -21,22 +21,19 @@ const copy = async (id: number): Promise<void> => {
     });
     return;
   };
+
   const data = puzzles.map((p) =>
     [p.name, p.answer || `${StatusEmoji(p.status)} ${StatusLabel(p.status)}`]);
   const text = data.map(([a, b]) => `${a}, ${b}`).join("\n");
   const html = "<table>\n" + data.map(([a, b]) =>
     `<tr><td>${a}</td><td>${b}</td></tr>`
   ).join("\n") + "</table>";
-  if (navigator.clipboard.write) {
-    await navigator.clipboard.write([
-      new ClipboardItem({
-        "text/plain": new Blob([text], { type: "text/plain" }),
-        "text/html": new Blob([html], { type: "text/html" }),
-      }),
-    ]);
-  } else {
-    await navigator.clipboard.writeText(text);
-  }
+  await navigator.clipboard.write([
+    new ClipboardItem({
+      "text/plain": new Blob([text], { type: "text/plain" }),
+      "text/html": new Blob([html], { type: "text/html" }),
+    }),
+  ]);
   toast.add({
     title: "Copied",
     color: "green",
