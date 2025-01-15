6359a43370ce44130b1d0a0674dc7d1408e27009
Benjamin Tidor <benjamin@tidor.net>
2025-01-15 11:29:16 -0800

    JS: fix SSR with BroadcastChannel

--- app/composables/useMute.ts
+++ app/composables/useMute.ts
@@ -1,16 +1,20 @@
 const key = "muted";
 
 export default function (): [Ref<boolean>, (v: boolean) => void] {
+  let channel: BroadcastChannel | undefined;
+
   const mute = ref(false);
-  onMounted(() => mute.value = (localStorage.getItem(key) === "1"));
-
-  const channel = (new BroadcastChannel(key));
-  channel.addEventListener("message", (e) => (mute.value = !!e.data));
-
   const setMute = (v: boolean) => {
     mute.value = v;
     localStorage.setItem(key, v ? "1" : "0");
-    channel.postMessage(v);
+    channel?.postMessage(v);
   };
+
+  onMounted(() => {
+    mute.value = (localStorage.getItem(key) === "1");
+
+    channel = new BroadcastChannel(key);
+    channel.addEventListener("message", (e) => (mute.value = !!e.data));
+  });
   return [mute, setMute];
 }
